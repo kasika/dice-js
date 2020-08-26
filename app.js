73fd331970549f2ee9bde0080985cebdd8e3f0a9
Kasia Kowalska <kovalskyyyyy@gmail.com>
2020-08-25 22:45:59 -0300

    Fix player active class toggling

--- app.js
+++ app.js
@@ -33,20 +33,22 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
   // 3. Update score
   if (dice !== 1) {
+    
     //Add score
     roundScore += dice;
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
   } else {
+    
     //Next player
     // nextPlayer(); d.b.d later
+    
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
 
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
 
-    document.querySelector('.player-0-panel').classList.remove('active');
-    document.querySelector('.player-1-panel').classList.add('active');
-
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
   }
 });
